36b3978848b5b686acdd8ff1e2d8f616a4d8bd35
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-12-25 22:10:32 -0500

    fix: fix armeabi syscall code
    Things have changed since `syscall`/`sc` were first developed, and it
    appears the old code no longer works.

--- src/platform/linux-armeabi/mod.rs
+++ src/platform/linux-armeabi/mod.rs
@@ -14,7 +14,7 @@ pub mod nr;
 #[inline(always)]
 pub unsafe fn syscall0(n: usize) -> usize {
     let ret: usize;
-    llvm_asm!("swi $$0"
+    llvm_asm!("swi #0"
          : "={r0}"(ret)
          : "{r7}"(n)
          : "memory" "cc"
@@ -25,7 +25,7 @@ pub unsafe fn syscall0(n: usize) -> usize {
 #[inline(always)]
 pub unsafe fn syscall1(n: usize, a1: usize) -> usize {
     let ret: usize;
-    llvm_asm!("swi $$0"
+    llvm_asm!("swi #0"
          : "={r0}"(ret)
          : "{r7}"(n) "{r0}"(a1)
          : "memory" "cc"
@@ -36,7 +36,7 @@ pub unsafe fn syscall1(n: usize, a1: usize) -> usize {
 #[inline(always)]
 pub unsafe fn syscall2(n: usize, a1: usize, a2: usize) -> usize {
     let ret: usize;
-    llvm_asm!("swi $$0"
+    llvm_asm!("swi #0"
          : "={r0}"(ret)
          : "{r7}"(n) "{r0}"(a1) "{r1}"(a2)
          : "memory" "cc"
@@ -47,7 +47,7 @@ pub unsafe fn syscall2(n: usize, a1: usize, a2: usize) -> usize {
 #[inline(always)]
 pub unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> usize {
     let ret: usize;
-    llvm_asm!("swi $$0"
+    llvm_asm!("swi #0"
          : "={r0}"(ret)
          : "{r7}"(n) "{r0}"(a1) "{r1}"(a2) "{r2}"(a3)
          : "memory" "cc"
@@ -58,7 +58,7 @@ pub unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> usize {
 #[inline(always)]
 pub unsafe fn syscall4(n: usize, a1: usize, a2: usize, a3: usize, a4: usize) -> usize {
     let ret: usize;
-    llvm_asm!("swi $$0"
+    llvm_asm!("swi #0"
          : "={r0}"(ret)
          : "{r7}"(n) "{r0}"(a1) "{r1}"(a2) "{r2}"(a3) "{r3}"(a4)
          : "memory" "cc"
@@ -69,7 +69,7 @@ pub unsafe fn syscall4(n: usize, a1: usize, a2: usize, a3: usize, a4: usize) -> 
 #[inline(always)]
 pub unsafe fn syscall5(n: usize, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize) -> usize {
     let ret: usize;
-    llvm_asm!("swi $$0" : "={r0}"(ret)
+    llvm_asm!("swi #0" : "={r0}"(ret)
          : "{r7}"(n) "{r0}"(a1) "{r1}"(a2) "{r2}"(a3) "{r3}"(a4) "{r4}"(a5)
          : "memory" "cc"
          : "volatile");
@@ -87,7 +87,7 @@ pub unsafe fn syscall6(
     a6: usize,
 ) -> usize {
     let ret: usize;
-    llvm_asm!("swi $$0"
+    llvm_asm!("swi #0"
          : "={r0}"(ret)
          : "{r7}"(n) "{r0}"(a1) "{r1}"(a2) "{r2}"(a3) "{r3}"(a4) "{r4}"(a5)
            "{r5}"(a6)
@@ -108,7 +108,7 @@ pub unsafe fn syscall7(
     a7: usize,
 ) -> usize {
     let ret: usize;
-    llvm_asm!("swi $$0"
+    llvm_asm!("swi #0"
          : "={r0}"(ret)
          : "{r7}"(n) "{r0}"(a1) "{r1}"(a2) "{r2}"(a3) "{r3}"(a4) "{r4}"(a5)
            "{r5}"(a6) "{r6}"(a7)
