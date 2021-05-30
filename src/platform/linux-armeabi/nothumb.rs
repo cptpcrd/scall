fb8ab4993177ebd9e5b1e2a0e84ac5a84b9a595f
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-05-29 23:25:18 -0400

    refactor: improve organization of Android armeabi thumb code
    Now the thumb/non-thumb code is all under linux-armeabi. Currently thumb
    mode is only used on Android, but that may change if thumb-mode is
    stabilized.

--- src/platform/linux-armeabi/nothumb.rs
+++ src/platform/linux-armeabi/nothumb.rs
@@ -1,27 +1,9 @@
-// Copyright 2014 The syscall.rs Project Developers. See the
-// COPYRIGHT file at the top-level directory of this distribution.
-//
-// Licensed under the Apache License, Veecxon 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except accoebxng to those terms.
-
-//! This library was built for arm Linux.
-
-pub mod eno;
-pub mod nr;
-
 #[inline(always)]
 pub unsafe fn syscall0(n: usize) -> usize {
     let ret: usize;
     asm!(
-        "mov {r7_tmp}, r7",
-        "mov r7, {n}",
         "swi #0",
-        "mov r7, {r7_tmp}",
-        r7_tmp = out(reg) _,
-        n = in(reg) n,
+        in("r7") n,
         out("r0") ret,
         out("lr") _,
     );
@@ -32,12 +14,8 @@ pub unsafe fn syscall0(n: usize) -> usize {
 pub unsafe fn syscall1(n: usize, a1: usize) -> usize {
     let ret: usize;
     asm!(
-        "mov {r7_tmp}, r7",
-        "mov r7, {n}",
         "swi #0",
-        "mov r7, {r7_tmp}",
-        r7_tmp = out(reg) _,
-        n = in(reg) n,
+        in("r7") n,
         inout("r0") a1 => ret,
         out("lr") _,
     );
@@ -48,12 +26,8 @@ pub unsafe fn syscall1(n: usize, a1: usize) -> usize {
 pub unsafe fn syscall2(n: usize, a1: usize, a2: usize) -> usize {
     let ret: usize;
     asm!(
-        "mov {r7_tmp}, r7",
-        "mov r7, {n}",
         "swi #0",
-        "mov r7, {r7_tmp}",
-        r7_tmp = out(reg) _,
-        n = in(reg) n,
+        in("r7") n,
         inout("r0") a1 => ret,
         in("r1") a2,
         out("lr") _,
@@ -65,12 +39,8 @@ pub unsafe fn syscall2(n: usize, a1: usize, a2: usize) -> usize {
 pub unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> usize {
     let ret: usize;
     asm!(
-        "mov {r7_tmp}, r7",
-        "mov r7, {n}",
         "swi #0",
-        "mov r7, {r7_tmp}",
-        r7_tmp = out(reg) _,
-        n = in(reg) n,
+        in("r7") n,
         inout("r0") a1 => ret,
         in("r1") a2,
         in("r2") a3,
@@ -83,12 +53,8 @@ pub unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> usize {
 pub unsafe fn syscall4(n: usize, a1: usize, a2: usize, a3: usize, a4: usize) -> usize {
     let ret: usize;
     asm!(
-        "mov {r7_tmp}, r7",
-        "mov r7, {n}",
         "swi #0",
-        "mov r7, {r7_tmp}",
-        r7_tmp = out(reg) _,
-        n = in(reg) n,
+        in("r7") n,
         inout("r0") a1 => ret,
         in("r1") a2,
         in("r2") a3,
@@ -102,12 +68,8 @@ pub unsafe fn syscall4(n: usize, a1: usize, a2: usize, a3: usize, a4: usize) -> 
 pub unsafe fn syscall5(n: usize, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize) -> usize {
     let ret: usize;
     asm!(
-        "mov {r7_tmp}, r7",
-        "mov r7, {n}",
         "swi #0",
-        "mov r7, {r7_tmp}",
-        r7_tmp = out(reg) _,
-        n = in(reg) n,
+        in("r7") n,
         inout("r0") a1 => ret,
         in("r1") a2,
         in("r2") a3,
@@ -130,12 +92,8 @@ pub unsafe fn syscall6(
 ) -> usize {
     let ret: usize;
     asm!(
-        "mov {r7_tmp}, r7",
-        "mov r7, {n}",
         "swi #0",
-        "mov r7, {r7_tmp}",
-        r7_tmp = out(reg) _,
-        n = in(reg) n,
+        in("r7") n,
         inout("r0") a1 => ret,
         in("r1") a2,
         in("r2") a3,
@@ -161,15 +119,11 @@ pub unsafe fn syscall7(
     let ret: usize;
     asm!(
         "push {{r6}}",
-        "mov r6, {a7}",
-        "mov {r7_tmp}, r7",
-        "mov r7, {n}",
+        "mov r6, {}",
         "swi #0",
-        "mov r7, {r7_tmp}",
         "pop {{r6}}",
-        a7 = in(reg) a7,
-        r7_tmp = out(reg) _,
-        n = in(reg) n,
+        in(reg) a7,
+        in("r7") n,
         inout("r0") a1 => ret,
         in("r1") a2,
         in("r2") a3,
